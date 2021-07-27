0118061b05fc83d9f41ba55b3755f7cea1ca789f
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2021-07-27 20:57:50 +0200

    Add files via upload

---
+++ src/main/java/khaosmc/discord/chatbridge/DiscordChatBridgeMod.java
@@ -0,0 +1,11 @@
+package khaosmc.discord.chatbridge;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DiscordChatBridgeMod implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		
+	}
+}
